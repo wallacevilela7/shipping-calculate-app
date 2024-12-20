f8c6003961b0f9c7429de630545d0b5b03c6044c
Wallace Vilela <wallace.vilela9@gmail.com>
2024-12-20 16:37:14 -0300

    create app

---
+++ src/main/java/wvsdev/freteflexapp/FreteflexAppApplication.java
@@ -0,0 +1,13 @@
+package wvsdev.freteflexapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteflexAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreteflexAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wvsdev/freteflexapp/FreteflexAppApplicationTests.java
@@ -0,0 +1,13 @@
+package wvsdev.freteflexapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteflexAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
